383b0a3f77b19e10784c45b5d7a774d6524e4e78
Lithasa <lithasa0417@gmail.com>
2024-12-01 08:02:44 +0530

    php

---
+++ index.php
@@ -0,0 +1,72 @@
+
+<form action="index.php" method="post">
+    <input type="text" name="name">
+	<input type="text" name="id">
+	<input type="text" name="address">
+	<input type="text" name="contact">
+	<input type="submit" name="submit">
+</form>
+
+
+
+<?php
+
+
+
+//echo "<h1>Hi everyone,I am lithasa </h1>";
+
+//echo "<script>alert('hi')</script>";
+
+/*for($x=1; $x<6; $x++){
+	
+	echo "<h".$x.">Hello world</h".$x.">";
+}*/
+/*if(isset($_POST['submit'])){
+	
+$name = $_POST['name'];
+$id = $_POST['id'];
+$address = $_POST['address'];
+$contact = $_POST['contact'];
+
+	
+$conn = mysqli_connect("localhost","root","","lithasa_db");
+
+$sql = "INSERT INTO student values('$name',$id,'$address',$contact)";
+
+mysqli_query($conn,$sql);
+
+}
+*/
+/* $name = "lithasa";
+$name = "jayamaha";
+var_dump($name);
+*/
+
+
+// echo $name;
+
+
+//constants
+/*
+define("name","nimal");
+  $name = "bimal";
+  echo name;
+  
+ */
+ // const car = "BMW";
+ // echo car;
+ 
+ /*
+ if(19>17){
+	  define("a", true);
+ }
+ echo a;
+  
+  
+ */
+  
+
+
+
+
+?>
